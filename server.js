3a7b2b7c39949f16eecdeb061d45654372ca9b78
HN1508 <88259896+HN1508@users.noreply.github.com>
2025-12-13 00:54:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
